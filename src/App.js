124a53785766b4475b62bf6ff9eb1054aae69300
Doosik Jung <entlrsla@gmail.com>
2022-03-23 16:05:06 +0900

    시간변환,useState사용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState } from 'react';
 
-function App() {
+function MinutesToHour() {
+  const [amount, setAmount] = React.useState();
+  const [flipped, setFlipped] = React.useState(false);
+  const onChange = (e) => {
+    setAmount(e.target.value);
+  };
+  const reSet = () => setAmount("");
+
+  const onFlip = () => {
+    reSet();
+    setFlipped((current) => !current);
+  }
+  console.log(flipped);
+  console.log(!flipped);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <label htmlFor='minutes'>Minutes</label>
+        <input
+          value={flipped ? amount * 60 : amount}
+          id="minutes"
+          placeholder='Minutes'
+          type="number"
+          onChange={onChange}
+          disabled={flipped}
+        />
+      </div>
+      <h4>You want to convert {amount}</h4>
+      <div>
+        <label htmlFor='hours'>Hours</label>
+        <input
+          value={flipped ? amount : Math.round(amount / 60)}
+          id="hours"
+          placeholder='Hours'
+          type="number"
+          disabled={!flipped}
+          onChange={onChange}
+        />
+      </div>
+      <button onClick={reSet}>Reset</button>
+      <button onClick={onFlip}>{flipped ? 'minutes' : 'hours'}</button>
     </div>
   );
+
+}
+function KmToMiles() {
+  return <h3>KM 2 M</h3>;
+}
+function App() {
+  const [index, setIndex] = React.useState(0);
+  const onSelect = (e) => {
+    setIndex(e.target.value);
+    console.log(index);
+  };
+
+  return (
+    <div>
+      <h1>Spuer Converter</h1>
+      <select value={index} onChange={onSelect}>
+        <option value="0">Select Change Unit!!</option>
+        <option value="1">Minutes & Hours</option>
+        <option value="2">Km & Miles</option>
+      </select>
+      <hr></hr>
+      {index === "0" ? "Please select your units" : null}
+      {index === "1" ? <MinutesToHour /> : null}
+      {index === "2" ? <KmToMiles /> : null}
+    </div>
+  );
+
 }
 
 export default App;
